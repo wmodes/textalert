81d293ba8de09d17f7cc8e09c9dbc8a8b10bee13
Wes Modes <wmodes@gmail.com>
2017-03-15 19:04:26 -0700

    Stubbed out textalert.py

---
+++ textalert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""textalert.py: textalert system
+Authors: UBEW (ubew000@gmail.com)
+Copyright: 2017, MIT"""
+
+# -*- coding: iso-8859-15 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+#
+# imports
+#
+
+
+# local modules
+
+
+
+
+#
+# Control
+#
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
